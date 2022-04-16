ec949fd0cfbe7ec16a5fa48346aee248b19ede60
Sakshi <87997952+deepaiims@users.noreply.github.com>
2022-04-16 23:43:17 +0530

    Update evaluators.py

--- userbot/plugins/evaluators.py
+++ userbot/plugins/evaluators.py
@@ -4,10 +4,11 @@ import os
 import sys
 import traceback
 
-from ..helpers.utils import _format
+from ..helpers.utils import _format, json_parser
 from . import *
 
 plugin_category = "tools"
+thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg")
 
 
 @catub.cat_cmd(
@@ -45,7 +46,8 @@ async def _(event):
     )
     if BOTLOG:
         await event.client.send_message(
-            BOTLOG_CHATID, f"Terminal command {cmd} was executed sucessfully."
+            BOTLOG_CHATID,
+            "Terminal command " + cmd + " was executed sucessfully.",
         )
 
 
@@ -74,35 +76,39 @@ async def _(event):
     redirected_output = sys.stdout = io.StringIO()
     redirected_error = sys.stderr = io.StringIO()
     stdout, stderr, exc = None, None, None
+    reply_to_id = await reply_id(event)
     try:
-        await aexec(cmd, event)
+        value = await aexec(cmd, event)
     except Exception:
         exc = traceback.format_exc()
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
     sys.stdout = old_stdout
     sys.stderr = old_stderr
-    evaluation = ""
-    if exc:
-        evaluation = exc
-    elif stderr:
-        evaluation = stderr
-    elif stdout:
-        evaluation = stdout
-    else:
-        evaluation = "Success"
+    evaluation = exc or stderr or stdout or _parse_eval(value) or "Success"
     final_output = (
-        f"**•  Eval : **\n```{cmd}``` \n\n**•  Result : **\n```{evaluation}``` \n"
+        f"__►__ **Eval : **\n```{cmd}``` \n\n__►__ **Result : **\n```{evaluation}``` \n"
     )
-    await edit_or_reply(
-        catevent,
-        text=final_output,
-        aslink=True,
-        linktext=f"**•  Eval : **\n```{cmd}``` \n\n**•  Result : **\n",
-    )
+    if len(final_output) > 4096:
+        neko = final_output.replace("`", "").replace("**", "").replace("__", "")
+        with io.BytesIO(str.encode(neko)) as out_file:
+            out_file.name = "eval.txt"
+            await event.client.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                thumb=thumb_image_path,
+                allow_cache=False,
+                caption=f"```{cmd}```" if len(cmd) < 998 else None,
+                reply_to=reply_to_id,
+            )
+        return await catevent.delete()
+    await edit_or_reply(event, final_output)
+
     if BOTLOG:
         await event.client.send_message(
-            BOTLOG_CHATID, f"eval command {cmd} was executed sucessfully."
+            BOTLOG_CHATID,
+            "eval command " + cmd + " was executed sucessfully.",
         )
 
 
@@ -110,13 +116,31 @@ async def aexec(code, smessatatus):
     message = event = smessatatus
     p = lambda _x: print(_format.yaml_format(_x))
     reply = await event.get_reply_message()
+    r = reply
     exec(
         (
-            "async def __aexec(message, event , reply, client, p, chat): "
+            "async def __aexec(message, event , reply, r, client, p, chat): "
             + "".join(f"\n {l}" for l in code.split("\n"))
         )
     )
 
     return await locals()["__aexec"](
-        message, event, reply, message.client, p, message.chat_id
+        message, event, reply, r, message.client, p, message.chat_id
     )
+
+
+def _parse_eval(value):
+    if value is None:
+        return
+    if hasattr(value, "stringify"):
+        try:
+            return value.stringify()
+        except TypeError:
+            pass
+    elif isinstance(value, dict):
+        try:
+            return json_parser(value, indent=4)
+        except BaseException:
+            pass
+    # is to_dict is also Good option to format?
+    return str(value)
