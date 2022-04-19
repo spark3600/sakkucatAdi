f745c08f65197793d3bd238fc3f75ad0c5526c19
Sakshi <87997952+deepaiims@users.noreply.github.com>
2022-04-19 22:54:31 +0530

    Update tools.py

--- userbot/helpers/utils/tools.py
+++ userbot/helpers/utils/tools.py
@@ -95,3 +95,34 @@ async def take_screen_shot(
     return thumb_image_path if os.path.exists(thumb_image_path) else None
 
 
+def get_c_m_message(message_link: str) -> Tuple[Union[str, int], int]:
+    p_m_link = message_link.split("/")
+    chat_id, message_id = None, None
+    if len(p_m_link) == 6:
+        # private link
+        if p_m_link[3] == "c":
+            # for supergroups / channels
+            chat_id, message_id = int("-100" + p_m_link[4]), int(p_m_link[5])
+        elif p_m_link[4] == "UniBorg":
+            # for basic groups / private chats
+            chat_id, message_id = int(p_m_link[4]), int(p_m_link[5])
+    elif len(p_m_link) == 5:
+        # public link
+        chat_id, message_id = str("@" + p_m_link[3]), int(p_m_link[4])
+    return chat_id, message_id
+
+
+def json_parser(data, indent=None):
+    parsed = {}
+    try:
+        if isinstance(data, str):
+            parsed = json.loads(str(data))
+            if indent:
+                parsed = json.dumps(json.loads(str(data)), indent=indent)
+        elif isinstance(data, dict):
+            parsed = data
+            if indent:
+                parsed = json.dumps(data, indent=indent)
+    except JSONDecodeError:
+        parsed = eval(data)
+    return parsed
